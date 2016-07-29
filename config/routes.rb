1d451e2dc4d5e308909ad57bbf5ab04e1606933d
Shohei Saito <saito.ssh@gmail.com>
2016-07-29 12:02:13 +0900

    arrange path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :todos
+  root 'todos#index'
+
+  resources :todos do
+    member do
+      patch 'status'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
