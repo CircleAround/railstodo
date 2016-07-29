eaba697ddece05d731521be211373baae7b4c79d
Shohei Saito <saito.ssh@gmail.com>
2016-07-29 17:46:18 +0900

    add category_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729082629) do
+ActiveRecord::Schema.define(version: 20160729083639) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20160729082629) do
     t.string   "title"
     t.text     "body"
     t.integer  "status"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "priority",   default: 0, null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "priority",    default: 0, null: false
+    t.integer  "category_id"
   end
 
 end
